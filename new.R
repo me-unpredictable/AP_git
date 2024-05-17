72a48e3fdba44cdae28f63b4cd95576c579e08c5
me-unpredictable <vishalpatel654@gmail.com>
2024-05-17 18:11:12 +1000

    updating plot

--- new.R
+++ new.R
@@ -2,5 +2,3 @@ setwd('~/Documents/tmpgit/')
 data=read.csv('1.csv')
 data$t=as.Date(data$t,origin="2020-03-01")
 data_sub=subset(data,subset = t>="2020-07-01" & t<="2020-12-31")
-plot(data$t,data$H,type = 'l',col='RED',main='Covid Plot'
-     ,ylab='Hospital Beds',xlab='Dates')
